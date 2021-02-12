5e0de23e0f35e4228671c920ad2cf726669670fe
Jens Pfahl <jens.pfahl@mailbox.org>
2021-02-12 10:43:49 +0100

    make passwd dialog working

--- app/src/main/java/de/jepfa/yapm/ui/CredentialList.kt
+++ app/src/main/java/de/jepfa/yapm/ui/CredentialList.kt
@@ -4,6 +4,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -16,22 +18,43 @@ class CredentialListAdapter : ListAdapter<EncCredential, CredentialListAdapter.C
     val secretService = SecretService()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CredentialViewHolder {
-        return CredentialViewHolder.create(parent)
+        val holder = CredentialViewHolder.create(parent)
+        holder.listen {pos, type ->
+            val current = getItem(pos)
+            val key = secretService.getAndroidSecretKey("test-key")
+            val decAdditionalInfo = secretService.decryptCommonString(key, current.additionalInfo)
+            val password = secretService.decryptPassword(key, current.password)
+            AlertDialog.Builder(holder.itemView.context)
+                    .setTitle(decAdditionalInfo)
+                    .setMessage(password.debugToString())
+                    .show()
+            password.clear()
+        }
+
+        return holder
     }
 
     override fun onBindViewHolder(holder: CredentialViewHolder, position: Int) {
         val current = getItem(position)
         val key = secretService.getAndroidSecretKey("test-key")
-        val encName = secretService.decryptCommonString(key, current.name)
-        holder.bind(encName)
+        val decName = secretService.decryptCommonString(key, current.name)
+        holder.bind(decName)
+
     }
 
     class CredentialViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val credentialItemView: TextView = itemView.findViewById(R.id.textView)
 
+        fun listen(event: (position: Int, type: Int) -> Unit) {
+            credentialItemView.setOnClickListener {
+                event.invoke(adapterPosition, itemViewType)
+            }
+        }
+
         fun bind(text: CharSequence?) {
             credentialItemView.text = text
         }
+
 
         companion object {
             fun create(parent: ViewGroup): CredentialViewHolder {
