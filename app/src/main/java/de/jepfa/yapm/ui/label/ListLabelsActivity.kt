01f950b1d8cc8becabb1785562e497d2be4a0fc7
Jens Pfahl <jens.pfahl@mailbox.org>
2021-10-15 22:00:06 +0200

    delete all unused lables

--- app/src/main/java/de/jepfa/yapm/ui/label/ListLabelsActivity.kt
+++ app/src/main/java/de/jepfa/yapm/ui/label/ListLabelsActivity.kt
@@ -1,15 +1,24 @@
 package de.jepfa.yapm.ui.label
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.MotionEvent
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import de.jepfa.yapm.R
+import de.jepfa.yapm.model.Session
 import de.jepfa.yapm.service.label.LabelFilter
 import de.jepfa.yapm.service.label.LabelService
 import de.jepfa.yapm.ui.SecureActivity
+import de.jepfa.yapm.ui.nfc.NfcActivity
+import de.jepfa.yapm.ui.qrcode.QrCodeActivity
+import de.jepfa.yapm.usecase.LockVaultUseCase
+import de.jepfa.yapm.util.PermissionChecker
+import de.jepfa.yapm.util.putEncryptedExtra
 
 class ListLabelsActivity : SecureActivity() {
 
@@ -55,6 +64,59 @@ class ListLabelsActivity : SecureActivity() {
                 true
             }
             true
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        if (checkSession && Session.isDenied()) {
+            return false
+        }
+
+        menuInflater.inflate(R.menu.list_labels_menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (checkSession && Session.isDenied()) {
+            LockVaultUseCase.execute(this)
+            return false
+        }
+
+        if (id == R.id.menu_delete_unused_labels) {
+            AlertDialog.Builder(this)
+                .setTitle(getString(R.string.delete_unused_labels))
+                .setMessage(getString(R.string.message_delete_unused_labels))
+                .setIcon(R.drawable.ic_baseline_label_24)
+                .setPositiveButton(android.R.string.yes) { dialog, _ ->
+                    deleteUnusedLabels()
+                    dialog.dismiss()
+                }
+                .setNegativeButton(android.R.string.no, null)
+                .show()
+            return true
+        }
+
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun deleteUnusedLabels() {
+        LabelService.getAllLabels()
+            .forEach { label ->
+                val labelId = label.labelId
+                if (labelId != null) {
+                    val notUsed =
+                        LabelService.getCredentialIdsForLabelId(labelId)?.isEmpty() ?: true
+                    if (notUsed) {
+                        label.let {
+                            deleteLabel(it)
+                        }
+                    }
+                }
         }
 
     }
